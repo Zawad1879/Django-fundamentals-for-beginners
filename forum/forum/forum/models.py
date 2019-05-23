329debcb22c3bc70281b22975fd762bc8a7996e5
Niamat Zawad <zawad@level-five.jp>
2019-05-23 11:40:48 +0900

    Add Comment model

--- forum/forum/forum/models.py
+++ forum/forum/forum/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-import datetime
+from datetime import datetime
 from django.contrib.auth.models import User
 # Create your models here.
 
@@ -8,7 +8,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	post_title = models.CharField(max_length = 200)
-	post_text = models.DateTimeField('date published')
+	post_text = models.CharField(max_length = 200)
+	pub_date = models.DateTimeField('date published', default=datetime.now())
 
 	def was_published_recently(self):
 		now = timezone.now()
@@ -16,3 +17,12 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.post_title
+
+class Comment(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, on_delete=models.CASCADE)
+	comment_text = models.CharField(max_length = 200)
+
+	def __str__(self):
+		return self.comment_text
+
